b92dfac62aca15f731e274c62da82f7b3def8042
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-02-09 20:16:01 -0500

    Refactor method for showcasing user's reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -88,7 +88,28 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+  const queryString = `SELECT properties.id, properties.title, properties.cost_per_night, reservations.start_date, AVG(property_reviews.rating) 
+  FROM reservations
+  JOIN properties
+  ON properties.id = reservations.property_id
+  JOIN property_reviews
+  ON properties.id = property_reviews.property_id
+  JOIN users
+  ON users.id = reservations.guest_id
+  WHERE users.id = $1
+  GROUP BY properties.id, reservations.start_date
+  ORDER BY start_date
+  LIMIT $2;`;
+
+  return pool.query(queryString,[guest_id,limit]).then((result) => {
+    //console.log(result.rows);
+    return result.rows;
+  }).catch((err)=> {
+    console.log(err)
+  });
+  
+  //return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
