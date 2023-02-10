84e5c7bbf0059af64f7b5136e39d99ebbde78ca1
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-02-10 00:28:03 -0500

    Add fix for GetAllReservation, add package modification

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -20,20 +20,13 @@ const getUserWithEmail = function(email) {
   return pool
     .query(`SELECT * FROM users
     WHERE users.email = $1 LIMIT 1;`,[email]).then((result) => {
-      //console.log("Testing",result.rows);
-      
     return result.rows[0];
-      // result.rows.forEach(user => {
-      //   console.log(user)
-      //   //return user;
-      //   return Promise.resolve(user);
-      // });
     }).catch((err)=> {
       console.log(err);
       return null;
     });
 
-  // return Promise.resolve(user);
+
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -89,7 +82,7 @@ exports.addUser = addUser;
  */
 const getAllReservations = function(guest_id, limit = 10) {
 
-  const queryString = `SELECT properties.id, properties.title, properties.cost_per_night, reservations.start_date, AVG(property_reviews.rating) 
+  const queryString = `SELECT properties.id, properties.title, properties.description, properties.cover_photo_url, properties.thumbnail_photo_url , properties.number_of_bedrooms , properties.number_of_bathrooms, properties.parking_spaces, properties.cost_per_night, reservations.start_date, AVG(property_reviews.rating) 
   FROM properties
   LEFT JOIN reservations
   ON properties.id = reservations.property_id
@@ -127,7 +120,7 @@ const getAllProperties = function(options, limit = 10) {
   
   let qString = `SELECT properties.*, AVG(property_reviews.rating) as average_rating FROM properties JOIN property_reviews ON properties.id = property_id `;
 
-  //console.log("Insider query there is",qString, typeof qString);
+
 
   if (options.owner_id) {
     qString += `JOIN users ON users.id = properties.owner_id `;
@@ -158,7 +151,7 @@ const getAllProperties = function(options, limit = 10) {
     qString += ` user.id = $${queryParams.length} `;
   }
   
-  // 4
+  
   queryParams.push(limit);
   qString += `
   GROUP BY properties.id
@@ -171,7 +164,7 @@ const getAllProperties = function(options, limit = 10) {
 
   return pool
     .query(qString,queryParams).then((result) => {
-      //console.log(result.rows);
+  
       return result.rows;
     }).catch((err)=> {
       console.log(err)
@@ -187,10 +180,7 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  // const propertyId = Object.keys(properties).length + 1;
-  // property.id = propertyId;
-  // properties[propertyId] = property;
-  // return Promise.resolve(property);
+
   let queryParams = [];
   let queryString = `INSERT INTO properties (
     title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code) 
